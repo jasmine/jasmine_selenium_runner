20725b2f1bd3179131dfbbca10741856e41f8539
Gregg Van Hove <pair+gvanhove@pivotallabs.com>
2013-09-27 12:17:14 -0700

    Use in_temp_dir helper for remaining integration tests

--- spec/selenium_runner_integration_spec.rb
+++ spec/selenium_runner_integration_spec.rb
@@ -7,49 +7,40 @@ require 'net/http'
 describe Jasmine::Runners::Selenium do
   let(:file_helper) { FileHelper.new }
   it "permits rake jasmine:ci task to be run using Selenium" do
-    project_root = File.expand_path(File.join(__FILE__, '..', '..'))
-    Dir.mktmpdir do |dir|
-      begin
-        Dir.chdir dir
-        File.open(File.join(dir, 'Gemfile'), 'w') do |file|
-          file.write <<-GEMFILE
+    in_temp_dir do |dir, project_root|
+      File.open(File.join(dir, 'Gemfile'), 'w') do |file|
+        file.write <<-GEMFILE
 source 'https://rubygems.org'
 gem 'jasmine_selenium_runner', :path => '#{project_root}'
 gem 'jasmine', :git => 'https://github.com/pivotal/jasmine-gem.git'
 GEMFILE
-        end
-        Bundler.with_clean_env do
-          `bundle`
-          `bundle exec jasmine init`
-          `bundle exec jasmine examples`
-          FileUtils.cp(File.join(project_root, 'spec', 'fixtures', 'is_in_firefox_spec.js'), File.join(dir, 'spec', 'javascripts'))
-          ci_output = `bundle exec rake -E "require 'jasmine_selenium_runner'" --trace jasmine:ci`
-          ci_output.should =~ /[1-9][0-9]* specs, 0 failures/
-        end
-      ensure
-        Dir.chdir project_root
+      end
+      Bundler.with_clean_env do
+        `bundle`
+        `bundle exec jasmine init`
+        `bundle exec jasmine examples`
+        FileUtils.cp(File.join(project_root, 'spec', 'fixtures', 'is_in_firefox_spec.js'), File.join(dir, 'spec', 'javascripts'))
+        ci_output = `bundle exec rake -E "require 'jasmine_selenium_runner'" --trace jasmine:ci`
+        ci_output.should =~ /[1-9][0-9]* specs, 0 failures/
       end
     end
   end
 
   it "permits rake jasmine:ci task to be run using Sauce", :sauce => true do
-    project_root = File.expand_path(File.join(__FILE__, '..', '..'))
-    Dir.mktmpdir do |dir|
-      begin
-        Dir.chdir dir
-        File.open(File.join(dir, 'Gemfile'), 'w') do |file|
-          file.write <<-GEMFILE
+    in_temp_dir do |dir, project_root|
+      File.open(File.join(dir, 'Gemfile'), 'w') do |file|
+        file.write <<-GEMFILE
 source 'https://rubygems.org'
 gem 'jasmine_selenium_runner', :path => '#{project_root}'
 gem 'jasmine', :git => 'https://github.com/pivotal/jasmine-gem.git'
 GEMFILE
-        end
-        Bundler.with_clean_env do
-          `bundle`
-          `bundle exec jasmine init`
-          `bundle exec jasmine examples`
-          File.open(File.join(dir, 'spec', 'javascripts', 'support', 'jasmine_selenium_runner.yml'), 'w') do |file|
-            file.write <<-YAML
+      end
+      Bundler.with_clean_env do
+        `bundle`
+        `bundle exec jasmine init`
+        `bundle exec jasmine examples`
+        File.open(File.join(dir, 'spec', 'javascripts', 'support', 'jasmine_selenium_runner.yml'), 'w') do |file|
+          file.write <<-YAML
 ---
 use_sauce: true
 browser: "internet explorer"
@@ -66,27 +57,24 @@ sauce:
   os: "Windows 8"
   browser_version: 10
 YAML
-          end
-
-          FileUtils.cp(File.join(project_root, 'spec', 'fixtures', 'is_in_ie_spec.js'), File.join(dir, 'spec', 'javascripts'))
-
-          test_start_time = Time.now.to_i
-          uri = URI.parse "https://saucelabs.com/rest/v1/#{ENV['SAUCE_USERNAME']}/jobs?from=#{test_start_time}"
-          job_list_request = Net::HTTP::Get.new(uri)
-          job_list_request.basic_auth(ENV['SAUCE_USERNAME'], ENV['SAUCE_ACCESS_KEY'])
-          before = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-            http.request(job_list_request)
-          end
-          JSON.parse(before.body).should == []
-          ci_output = %x{bundle exec rake -E "require 'jasmine_selenium_runner'" --trace jasmine:ci}
-          ci_output.should =~ (/[1-9][0-9]* specs, 0 failures/)
-          after = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-            http.request(job_list_request)
-          end
-          JSON.parse(after.body).should_not be_empty
         end
-      ensure
-        Dir.chdir project_root
+
+        FileUtils.cp(File.join(project_root, 'spec', 'fixtures', 'is_in_ie_spec.js'), File.join(dir, 'spec', 'javascripts'))
+
+        test_start_time = Time.now.to_i
+        uri = URI.parse "https://saucelabs.com/rest/v1/#{ENV['SAUCE_USERNAME']}/jobs?from=#{test_start_time}"
+        job_list_request = Net::HTTP::Get.new(uri)
+        job_list_request.basic_auth(ENV['SAUCE_USERNAME'], ENV['SAUCE_ACCESS_KEY'])
+        before = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+          http.request(job_list_request)
+        end
+        JSON.parse(before.body).should == []
+        ci_output = %x{bundle exec rake -E "require 'jasmine_selenium_runner'" --trace jasmine:ci}
+        ci_output.should =~ (/[1-9][0-9]* specs, 0 failures/)
+        after = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+          http.request(job_list_request)
+        end
+        JSON.parse(after.body).should_not be_empty
       end
     end
   end
